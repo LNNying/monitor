e9aef7d4b9bc9f22d006e9a655b5966ec0f7f288
liuningning <1754102225@qq.com>
2020-03-24 16:10:12 +0800

    监控项目

---
+++ 智能仓监控/www_wcs/config/config.js
@@ -0,0 +1,19 @@
+// 备用配置文件
+dataVideo = [
+    {
+        name: "video1",
+        value: 'http://localhost:9623/stream'
+    },
+    {
+        name: "video2",
+        value: 'http://localhost:9625/stream'
+    },
+    {
+        name: "video3",
+        value: 'http://localhost:9626/stream'
+    },
+    {
+        name: "video4",
+        value: 'http://localhost:9627/stream'
+    }
+];
